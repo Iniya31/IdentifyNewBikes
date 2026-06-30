1e749f9d9d10387ed030ed467bfdb6b419fe169b
sabarnashinchu <sabarnashinchu@gmail.com>
2026-06-30 14:27:28 +0530

    config.properties added with tested

--- src/test/java/basetest/BaseTest.java
+++ src/test/java/basetest/BaseTest.java
@@ -1,4 +1,23 @@
 package basetest;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import utilities.DriverSetup;
+
 public class BaseTest {
+    public WebDriver driver;
+    @Parameters("browser")
+    @BeforeClass
+    public void setup(String browser) {
+        driver = DriverSetup.getDriver(browser);
+        System.out.println("Browser Opened Successfully");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        DriverSetup.quitDriver(driver);
+        System.out.println("Browser Closed Successfully");
+    }
 }
