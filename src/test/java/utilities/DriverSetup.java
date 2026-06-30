f8c864514c41d7bb746aec9eb79d6a684858256c
Iniya31 <iniyanarayanasamy31@gmail.com>
2026-06-30 14:26:31 +0530

    1

--- src/test/java/utilities/DriverSetup.java
+++ src/test/java/utilities/DriverSetup.java
@@ -8,25 +8,20 @@ public class DriverSetup {
     public static WebDriver driver;
 
     public static WebDriver getDriver(String browser) {
-        if (driver == null) {
 
-            if (browser.equalsIgnoreCase("chrome")) {
-                driver = new ChromeDriver();
-            } else if (browser.equalsIgnoreCase("edge")) {
-                driver = new EdgeDriver();
-            } else {
-                throw new IllegalArgumentException("Unsupported browser: " + browser);
-            }
-            driver.manage().window().maximize();
-            driver.manage().deleteAllCookies();
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
         return driver;
     }
 
     public static void quitDriver() {
-        if(driver != null){
             driver.close();
-            driver = null;
-        }
     }
 }
