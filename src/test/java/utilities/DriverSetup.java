a58b1ce75d7422e5e88ae64036168cb70c7c22e2
sabarnashinchu <sabarnashinchu@gmail.com>
2026-06-30 14:27:28 +0530

    config.properties added with tested

--- src/test/java/utilities/DriverSetup.java
+++ src/test/java/utilities/DriverSetup.java
@@ -5,23 +5,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverSetup {
-    public static WebDriver driver;
-
     public static WebDriver getDriver(String browser) {
-
+        WebDriver driver;
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
-        } else if (browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
-        } else {
-            throw new IllegalArgumentException("Unsupported browser: " + browser);
         }
+        else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+        }
+        else {
+            throw new RuntimeException("Invalid Browser Name");
+        }
+        String url = ReadProperties.readProperty("baseUrl");
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
+        driver.get(url);
+        System.out.println("Browser Opened Successfully");
         return driver;
     }
 
-    public static void quitDriver() {
-            driver.close();
+    public static void quitDriver(WebDriver driver) {
+            driver.quit();
+            System.out.println("Browser Closed Successfully");
+
     }
 }
