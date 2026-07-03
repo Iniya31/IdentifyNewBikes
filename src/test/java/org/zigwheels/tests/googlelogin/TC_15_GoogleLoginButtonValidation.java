6faa9432910a58c6bf22752b2817348f2c04f5a5
Iniya31 <iniyanarayanasamy31@gmail.com>
2026-07-03 11:16:03 +0530

    Google login test added

--- src/test/java/org/zigwheels/tests/googlelogin/TC_15_GoogleLoginButtonValidation.java
+++ src/test/java/org/zigwheels/tests/googlelogin/TC_15_GoogleLoginButtonValidation.java
@@ -1,4 +1,24 @@
 package org.zigwheels.tests.googlelogin;
 
-public class TC_15_GoogleLoginButtonValidation {
+import basetest.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.zigwheels.pages.LoginPage;
+import utilities.Log;
+
+public class TC_15_GoogleLoginButtonValidation extends BaseTest {
+
+    @Test
+    public void verifyGoogleLoginButton() {
+        Log.info("Google Login Button Validation Started");
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.clickLoginRegister();
+        Log.info("Login/Register Button Clicked Successfully");
+        Assert.assertTrue(loginPage.isGoogleLoginButtonVisible(), "Google sign-in button is not visible!");
+        Log.info("Google Sign-In Button Displayed Successfully");
+        Log.info("Google Login Button Validation Passed");
+        loginPage.closeOAuthAndReturnHome();
+        Log.info("Returned To Main Application Window Successfully");
+
+    }
 }
